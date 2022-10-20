613e50ba52d275a404b2a034bc818c4ce47469c3
khanghoang1210 <116246004+khanghoang1210@users.noreply.github.com>
2022-10-20 23:38:20 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Chúc bé Như iu dấu 20/10 dui dẻ nhéeeee!",
+  introDesc: `Trái đất vốn lạ thường\n
+    Mà sao em cứ đi nhầm đường\n
+    Lạc vào tim anh lẻ loi\n
+    Đằng sau chữ yêu đây là thương`,
+  btnIntro: "hihi",
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙",
+  desc: "Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ",
+  btnYes: "Gất yêu anhhhh <33",
+  btnNo: "Không, anh bị đin à >.<",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3",
+  btnReply: "Gửi cho anh <3",
+  reply: "Yêu anh nhứt chên đời này lun <3333333",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: "Đi cùng anh đến tận cùng thời gian nhé babiii :3",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.facebook.com/messages/t/100012385397511", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
